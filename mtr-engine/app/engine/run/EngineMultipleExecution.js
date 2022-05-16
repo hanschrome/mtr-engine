f2eef08b0d990d97b8ff84e4b75d12c5fd41fd29
Hans Castro <hanscastroadmn@gmail.com>
2022-05-16 19:23:52 +0200

    Added UseCase for multiple execution

---
+++ mtr-engine/app/engine/run/EngineMultipleExecution.js
@@ -0,0 +1,46 @@
+const IndexConfigurationAdapter = require('../../../domain/robot/engines/configuration/IndexConfigurationAdapter');
+const ExchangeBalanceResponseAdapter = require('../../../domain/exchange/response/model/ExchangeBalanceResponseAdapter');
+const ExchangePricesResponseAdapter = require('../../../domain/exchange/response/model/ExchangePricesResponseAdapter');
+const ActionCollection = require('../../../domain/exchange/action/ActionCollection');
+
+class EngineMultipleExecution {
+
+    executeByConfiguration() {
+        this.callBalanceAndPrices(this.evaluateInstances);
+    }
+
+    /**
+     * @return {IndexConfigurationAdapter}
+     */
+    getIndexConfigurationAdapter() {
+        return new IndexConfigurationAdapter({});
+    }
+
+    callBalanceAndPrices(callback) {
+        callback(
+            this.getIndexConfigurationAdapter(),
+            new ExchangeBalanceResponseAdapter({}),
+            new ExchangePricesResponseAdapter({}));
+    }
+
+    /**
+     * @param indexConfigurationAdapter {IndexConfigurationAdapter}
+     * @param exchangeBalanceResponseAdapter {ExchangeBalanceResponseAdapter}
+     * @param exchangePricesResponseAdapter {ExchangePricesResponseAdapter}
+     */
+    evaluateInstances(indexConfigurationAdapter,
+                      exchangeBalanceResponseAdapter,
+                      exchangePricesResponseAdapter
+                      ) {
+        this.evaluateActions(new ActionCollection([]));
+    }
+
+    /**
+     * @param actionCollection {ActionCollection}
+     */
+    evaluateActions(actionCollection) {
+
+    }
+}
+
+module.exports = EngineMultipleExecution;
