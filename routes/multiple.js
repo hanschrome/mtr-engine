49c432d178211585b2d86bcc8f0042e8548a18e9
Hans Castro <hanscastroadmn@gmail.com>
2022-05-16 18:48:41 +0200

    Added route file for multiple execution

---
+++ routes/multiple.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.send('Hello :) I will be the endpoint for multiple instance executing');
+});
