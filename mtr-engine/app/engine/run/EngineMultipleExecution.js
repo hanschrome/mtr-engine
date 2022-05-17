088c517083ec3fc9a1a011e2223c5290e9f93b2e
Hans Castro <hanscastroadmn@gmail.com>
2022-05-17 08:31:07 +0200

    Added calls to exchange in order to recover prices and balances

--- mtr-engine/app/engine/run/EngineMultipleExecution.js
+++ mtr-engine/app/engine/run/EngineMultipleExecution.js
@@ -2,8 +2,21 @@ const IndexConfigurationAdapter = require('../../../domain/robot/engines/configu
 const ExchangeBalanceResponseAdapter = require('../../../domain/exchange/response/model/ExchangeBalanceResponseAdapter');
 const ExchangePricesResponseAdapter = require('../../../domain/exchange/response/model/ExchangePricesResponseAdapter');
 const ActionCollection = require('../../../domain/exchange/action/ActionCollection');
+const ExchangeFactory = require('../../../domain/exchange/factory/ExchangeFactory');
 
 class EngineMultipleExecution {
+
+    /**
+     * @type {ExchangeRepository}
+     */
+    exchangeRepository = null;
+
+    /**
+     * @param exchangeRepository {ExchangeRepository}
+     */
+    constructor(exchangeRepository) {
+        this.exchangeRepository = exchangeRepository;
+    }
 
     executeByConfiguration() {
         this.callBalanceAndPrices(this.evaluateInstances);
@@ -17,10 +30,38 @@ class EngineMultipleExecution {
     }
 
     callBalanceAndPrices(callback) {
-        callback(
-            this.getIndexConfigurationAdapter(),
-            new ExchangeBalanceResponseAdapter({}),
-            new ExchangePricesResponseAdapter({}));
+        const exchangeFactory = new ExchangeFactory();
+        const ExchangeService = exchangeFactory.getByKey(this.exchangeRepository.active);
+        const exchangeService = new ExchangeService(this.exchangeRepository.getActive());
+
+        exchangeService.balance((errorBalance, exchangeBalance) => {
+            if (errorBalance) {
+                return this.exchangeErrorLogRepository.addLog(id, 'ERROR_REQUEST_BALANCE', {
+                    error: errorBalance,
+                    response: exchangeBalance,
+                    engine: engineKey,
+                });
+            }
+
+            const exchangeBalanceResponseAdapter = new ExchangeBalanceResponseAdapter(exchangeBalance);
+
+            exchangeService.prices([], (errorPrices, exchangePrices) => {
+                if (errorPrices) {
+                    return this.exchangeErrorLogRepository.addLog(id, 'ERROR_REQUEST_PRICES', {
+                        error: errorPrices,
+                        response: exchangePrices,
+                        engine: engineKey,
+                    });
+                }
+
+                const exchangePricesResponseAdapter = new ExchangePricesResponseAdapter(exchangePrices);
+
+                callback(
+                    this.getIndexConfigurationAdapter(),
+                    exchangeBalanceResponseAdapter,
+                    exchangePricesResponseAdapter);
+            });
+        });
     }
 
     /**
@@ -39,14 +80,26 @@ class EngineMultipleExecution {
      * @param actionCollection {ActionCollection}
      */
     evaluateActions(actionCollection) {
+        for (let i = 0; i < actionCollection.getActions().length; i++) {
+            this.evaluateAction(actionCollection.getActions()[i]).then((e, m) => {
+                if (e) {
+                    // !!
+                }
 
+                console.log(m);
+            });
+        }
     }
 
     /**
      * @param action {IAction}
      */
-    evaluateAction(action) {
-
+    async evaluateAction(action) {
+        try {
+            // perform the action on the exchange
+        } catch (e) {
+            // handle error
+        }
     }
 }
 
