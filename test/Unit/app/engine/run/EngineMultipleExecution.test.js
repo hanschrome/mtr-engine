e537d2530b3f36eb84b46eb5cd6873a83158519b
Hans <hanscastroadmn@gmail.com>
2022-05-23 05:14:47 +0000

    [Multiple] Added data provider calls on unit methods

--- test/Unit/app/engine/run/EngineMultipleExecution.test.js
+++ test/Unit/app/engine/run/EngineMultipleExecution.test.js
@@ -7,22 +7,32 @@ const EngineMultipleExecutionTest = describe('[App] EngineMultipleExecution', ()
     });
 
     test('evaluateInstances', () => {
+        const engineMultipleExecutionTestDataProvider = new EngineMultipleExecutionTestDataProvider();
+        const data = engineMultipleExecutionTestDataProvider.testEvaluateInstances();
 
     });
 
     test('evaluateActions', () => {
+        const engineMultipleExecutionTestDataProvider= new EngineMultipleExecutionTestDataProvider();
+        const data = engineMultipleExecutionTestDataProvider.testEvaluateActions();
 
     });
 
     test('evaluateAction', () => {
+        const engineMultipleExecutionTestDataProvider = new EngineMultipleExecutionTestDataProvider();
+        const data = engineMultipleExecutionTestDataProvider.testEvaluateActions();
 
     });
 
     test('checkCanBuy', () => {
+        const engineMultipleExecutionTestDataProvider = new EngineMultipleExecutionTestDataProvider();
+        const data = engineMultipleExecutionTestDataProvider.testCheckCanBuy();
 
     });
 
     test('checkCanSell', () => {
+        const engineMultipleExecutionTestDataProvider = new EngineMultipleExecutionTestDataProvider();
+        const data = engineMultipleExecutionTestDataProvider.testCheckCanSell();
 
     });
 });
