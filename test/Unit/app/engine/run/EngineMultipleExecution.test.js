2cf15dea5fde76278766ac17a15f73fa88a1d1c4
Hans <hanscastroadmn@gmail.com>
2022-05-23 05:41:42 +0000

    [Multiple] EngineMultipleExecution::getBalanceAndPrices is tested to return values 0

--- test/Unit/app/engine/run/EngineMultipleExecution.test.js
+++ test/Unit/app/engine/run/EngineMultipleExecution.test.js
@@ -1,9 +1,58 @@
 const {describe, expect, test} = require('@jest/globals');
 const EngineMultipleExecutionTestDataProvider = require('EngineMultipleExecutionTest.data-provider');
+const EngineMultipleExecution = require('../../../../../mtr-engine/app/engine/run/EngineMultipleExecution');
+const ExchangeBalanceResponseAdapter = require('../../../../../mtr-engine/domain/exchange/response/model/ExchangeBalanceResponseAdapter');
+const ExchangePricesResponseAdapter = require('../../../../../mtr-engine/domain/exchange/response/model/ExchangePricesResponseAdapter');
 
 const EngineMultipleExecutionTest = describe('[App] EngineMultipleExecution', () => {
     test('getBalanceAndPrices', () => {
+        const exchangeRepository = {};
+        const engineRepository = {};
+        const exchangeErrorLogRepository = {};
+        const exchangeHistoryLogRepository = {};
+        const exchangeService = {
+            balance: (callback) => {
+                callback(null, {
+                    "ETH": {
+                        "available": 0,
+                        "onOrder": 0,
+                    }
+                });
+            },
+            prices: (callback) => {
+                callback(null, {
+                    "ETHBUSD": 0
+                });
+            }
+        };
 
+        const engineMultipleExecution = new EngineMultipleExecution(
+            exchangeRepository,
+            engineRepository,
+            exchangeErrorLogRepository,
+            exchangeHistoryLogRepository,
+            exchangeService
+        );
+
+        const balanceAndPricesPromise = engineMultipleExecution.getBalanceAndPrices();
+
+        balanceAndPricesPromise.then((exchangeData) => {
+            /** @var ExchangeBalanceResponseAdapter */
+            const exchangeBalanceResponseAdapter = exchangeData.exchangeBalanceResponseAdapter;
+            /** @var ExchangePricesResponseAdapter */
+            const exchangePricesResponseAdapter = exchangeData.exchangePricesResponseAdapter;
+
+            const isExchangeBalanceResponseAdapter = exchangeBalanceResponseAdapter instanceof ExchangeBalanceResponseAdapter;
+            const isExchangePricesResponseAdapter = exchangePricesResponseAdapter instanceof ExchangePricesResponseAdapter;
+
+            expect(isExchangeBalanceResponseAdapter).toEqual(true); // I'm sure there's some toBeTrue or toBeTruly but I'm in a train without internet connection
+            expect(isExchangePricesResponseAdapter).toEqual(true);
+            expect(exchangeBalanceResponseAdapter.getAvailableBalanceByCoin('ETH')).toEqual(0);
+            expect(exchangePricesResponseAdapter.getPriceByMarket('ETHBUSD')).toEqual(0);
+        }).catch((error) => {
+            // @todo
+            throw error;
+        });
     });
 
     test('evaluateInstances', () => {
