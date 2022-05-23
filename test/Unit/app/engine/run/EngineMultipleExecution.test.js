84a227a039d16417768666e5818d071b7d832518
Hans <hanscastroadmn@gmail.com>
2022-05-23 05:06:24 +0000

    [Multiple] Added structure for unit testing EngineMultipleExecution.test.js use case

---
+++ test/Unit/app/engine/run/EngineMultipleExecution.test.js
@@ -0,0 +1,28 @@
+const {describe, expect, test} = require('@jest/globals');
+const EngineMultipleExecutionTestDataProvider = require('EngineMultipleExecutionTest.data-provider');
+
+const EngineMultipleExecutionTest = describe('[App] EngineMultipleExecution', () => {
+    test('getBalanceAndPrices', () => {
+
+    });
+
+    test('evaluateInstances', () => {
+
+    });
+
+    test('evaluateActions', () => {
+
+    });
+
+    test('evaluateAction', () => {
+
+    });
+
+    test('checkCanBuy', () => {
+
+    });
+
+    test('checkCanSell', () => {
+
+    });
+});
---
+++ test/Unit/app/engine/run/EngineMultipleExecutionTest.data-provider.js
@@ -0,0 +1,24 @@
+class EngineMultipleExecutionTestDataProvider {
+
+    testEvaluateInstances() {
+
+    }
+
+    testEvaluateActions() {
+
+    }
+
+    testEvaluateAction() {
+
+    }
+
+    testCheckCanBuy() {
+
+    }
+
+    testCheckCanSell() {
+
+    }
+}
+
+module.exports = EngineMultipleExecutionTestDataProvider;
